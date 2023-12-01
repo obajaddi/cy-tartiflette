567520c8aaa62a75705959060399d36f199eea15
Othmane Bajaddi <othmane.bajaddi@kyriba.com>
2023-12-01 19:51:35 +0100

    adding loginTest and login custom commands

--- cypress/e2e/imbd/signin.js
+++ cypress/e2e/imbd/signin.js
@@ -1,35 +1,13 @@
 /// <reference types="cypress" />
 
 describe('Testing sign in feature', () => {
-	beforeEach(() => {
-		cy.fixture('imbd-accounts.json').as('users')
-		cy.visit('https://www.imdb.com/')
-		cy.intercept({ resourceType: /xhr|fetch/ }, { log: false })
-		cy.title().should(
-			'contain',
-			'IMDb: Ratings, Reviews, and Where to Watch the Best Movies & TV Shows'
-		)
-		cy.url().should('contain', 'imdb')
-	})
-
-	it.only('I can login with a valid account', function () {
-		cy.get('.nav__userMenu > .ipc-btn--single-padding').click()
-		cy.get('#signin-options .imdb-logo').click()
-		cy.get('#ap_email').type(this.users.main.mail)
-		// Not a good practice
-		// Should use secrets in env variables like 'CYPRESS_password=secret'
-		// Then use "Cypress.env('password')""
-		cy.get('#ap_password').type(this.users.main.password, { log: false })
-		cy.get('#signInSubmit').click()
+	it('I can login with a valid account', function () {
+		cy.loginTest()
 		cy.contains(this.users.main.username).should('be.visible')
 	})
 
 	it('I can not login with wrong mail', function () {
-		cy.get('.nav__userMenu > .ipc-btn--single-padding').click()
-		cy.get('#signin-options .imdb-logo').click()
-		cy.get('#ap_email').type('wrongMail')
-		cy.get('#ap_password').type(this.users.main.password, { log: false })
-		cy.get('#signInSubmit').click()
+		cy.loginTest('wrongMail')
 		cy.get('#auth-error-message-box')
 			.should('be.visible')
 			.and('contain.text', 'There was a problem')
@@ -39,11 +17,7 @@ describe('Testing sign in feature', () => {
 	})
 
 	it('I can not login with wrong password', function () {
-		cy.get('.nav__userMenu > .ipc-btn--single-padding').click()
-		cy.get('#signin-options .imdb-logo').click()
-		cy.get('#ap_email').type(this.users.main.mail)
-		cy.get('#ap_password').type('wrongPassword')
-		cy.get('#signInSubmit').click()
+		cy.loginTest(this.users.main.mail, 'wrongPassword')
 		cy.get('#auth-error-message-box')
 			.should('be.visible')
 			.and('contain.text', 'There was a problem')
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,36 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+before(() => {
+	cy.intercept({ resourceType: /xhr|fetch/ }, { log: false })
+	cy.fixture('imbd-accounts.json').as('users')
+})
+
+beforeEach(() => {
+	cy.visitHomePage()
+})
+
+Cypress.Commands.add('visitHomePage', () => {
+	cy.visit('https://www.imdb.com/')
+	cy.title().should(
+		'contain',
+		'IMDb: Ratings, Reviews, and Where to Watch the Best Movies & TV Shows'
+	)
+	cy.url().should('contain', 'imdb')
+})
+
+Cypress.Commands.add(
+	'loginTest',
+	function (email = this.users.main.mail, password = this.users.main.password) {
+		cy.get('.nav__userMenu > .ipc-btn--single-padding').click()
+		cy.get('#signin-options .imdb-logo').click()
+		cy.get('#ap_email').type(email)
+		// Not a good practice
+		// Should use secrets in env variables like 'CYPRESS_password=secret'
+		// Then use "Cypress.env('password')""
+		cy.get('#ap_password').type(password, { log: false })
+		cy.get('#signInSubmit').click()
+	}
+)
+
+Cypress.Commands.add('login', function () {
+	cy.loginTest()
+	cy.contains(this.users.main.username).should('be.visible')
+})

9c61afb5a2f3c59d2bf47a5bb60fcc1db6e7e40f
Othmane Bajaddi <othmane.bajaddi@kyriba.com>
2023-12-01 10:00:23 +0100

    Installing cypress & configs + linter + formatter

---
+++ cypress.config.js
@@ -0,0 +1,29 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+	e2e: {
+		setupNodeEvents(on, config) {
+			// implement node event listeners here
+		},
+		// -- GLOBAL OPTIONS -- //
+		specPattern: 'cypress/e2e/**/*.{js,jsx,ts}',
+		//baseUrl: '',
+		includeShadowDom: true,
+		screenshotOnRunFailure: true,
+		retries: {
+			runMode: 0,
+			openMode: 0,
+		},
+		defaultCommandTimeout: 10 * 1000,
+		pageLoadTimeout: 30 * 1000,
+		// -- VIDEO -- //
+		video: false,
+		videoCompression: 32,
+		// -- BROWSER -- //
+		chromeWebSecurity: false,
+		//blockHosts: null,
+		// -- VIEWPORT -- //
+		viewportHeight: 720,
+		viewportWidth: 1280,
+	},
+})
